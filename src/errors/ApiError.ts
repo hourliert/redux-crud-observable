b2e008876a74ffeb59579ddf1d491c6acaac94c2
Thomas Hourlier <thomas.hourlier@cnode.fr>
2017-02-14 00:22:13 +0100

    [chore]: update correct coverage to codecob
    [refactor]: remove useless getters and setters

--- src/errors/ApiError.ts
+++ src/errors/ApiError.ts
@@ -1,19 +1,6 @@
 import { IApiError } from './interfaces';
 
 export default class ApiError extends Error implements IApiError {
-  get data(): any {
-    return this.data;
-  }
-
-  get status(): number {
-    return this.status;
-  }
-
-  set data(d: any) {
-    this.data = d;
-  }
-
-  set status(s: number) {
-    this.status = s;
-  }
+  public data: any;
+  public status: number;
 }
